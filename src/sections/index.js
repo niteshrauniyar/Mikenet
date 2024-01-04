f0b4f7722943fb09a82044d56771d3626ec05b82
niteshrauniyar <104880718+niteshrauniyar@users.noreply.github.com>
2024-01-03 20:04:50 -0800

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Poularproducts from "./Poularproducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    Poularproducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
